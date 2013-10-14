acd9de339dcfc068264d1bd93dec5fb03f8a80d9
David Johnson <dave@g4dpz.me.uk>
2013-10-14 21:22:08 +0100

    Added creative commons

--- src/main/java/uk/org/funcube/fcdw/server/dao/DataAccessObject.java
+++ src/main/java/uk/org/funcube/fcdw/server/dao/DataAccessObject.java
@@ -1,3 +1,9 @@
+// FUNcube Data Warehouse
+// Copyright 2013 (c) David A.Johnson, G4DPZ, AMSAT-UK
+// This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+// To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ or send a letter
+// to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+
 package uk.org.funcube.fcdw.server.dao;
 
 import javax.persistence.EntityNotFoundException;
