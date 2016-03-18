bc0b45eed72887e521b8ddc4ece0314e9e293c3b
Dave Johnson <dave@g4dpz.me.uk>
2016-03-18 19:44:11 +0000

    Implemented MinMax for UKube

--- src/main/java/uk/org/funcube/fcdw/service/rest/RealTimeFC2ServiceRestImpl.java
+++ src/main/java/uk/org/funcube/fcdw/service/rest/RealTimeFC2ServiceRestImpl.java
@@ -25,6 +25,7 @@ import uk.org.funcube.fcdw.server.dao.MinMaxDao;
 import uk.org.funcube.fcdw.server.dao.RealTimeDao;
 import uk.org.funcube.fcdw.server.dao.SatelliteStatusDao;
 import uk.org.funcube.fcdw.server.model.HexFrame;
+import uk.org.funcube.fcdw.server.model.MinMax;
 import uk.org.funcube.fcdw.server.model.RealTimeEntity;
 import uk.org.funcube.fcdw.server.model.SatelliteStatus;
 import uk.org.funcube.fcdw.server.model.UserEntity;
@@ -79,9 +80,11 @@ public class RealTimeFC2ServiceRestImpl extends AbstractService {
 		
 		this.satelliteId = theSatelliteId;
 		
-		satelliteId = (satelliteId != null) ? satelliteId : new Long(2L);
+		satelliteId = (satelliteId != null) ? satelliteId : new Long(1L);
 
 		final HexFrame latestHexFrame = hexFrameDao.getLatestFC2();
+        
+        List<MinMax> minmaxValues = minMaxDao.findBySatelliteId(satelliteId);
 		
 		RealTimeEntity realTimeEntity;
 		
@@ -139,28 +142,95 @@ public class RealTimeFC2ServiceRestImpl extends AbstractService {
 		List<ValMinMax> paValues = new ArrayList<ValMinMax>();
 		List<StringPair> swValues = new ArrayList<StringPair>();
 		
-		epsValues.add(new ValMinMax("Solar Array +Y Deployable +", String.format(MILLI_AMPS_FORMAT, realTimeFC2.getSolarArrayCurrent1PlusString()), N_A, N_A));
-		epsValues.add(new ValMinMax("Solar Array +Y Deployable -", String.format(MILLI_AMPS_FORMAT, realTimeFC2.getSolarArrayCurrent1MinusString()), N_A, N_A));
-		epsValues.add(new ValMinMax("Solar Array Y Fixed +", String.format(MILLI_AMPS_FORMAT, realTimeFC2.getSolarArrayCurrent2PlusString()), N_A, N_A));
-		epsValues.add(new ValMinMax("Solar Array Y Fixed -", String.format(MILLI_AMPS_FORMAT, realTimeFC2.getSolarArrayCurrent2MinusString()), N_A, N_A));
-		epsValues.add(new ValMinMax("Solar Array Z Fixed +", String.format(MILLI_AMPS_FORMAT, realTimeFC2.getSolarArrayCurrent3PlusString()), N_A, N_A));
-		epsValues.add(new ValMinMax("Solar Array Z Fixed -", String.format(MILLI_AMPS_FORMAT, realTimeFC2.getSolarArrayCurrent3MinusString()), N_A, N_A));
-		epsValues.add(new ValMinMax("Solar Array -Y Deployable +", String.format(MILLI_AMPS_FORMAT, realTimeFC2.getSolarArrayCurrent4PlusString()), N_A, N_A));
-		epsValues.add(new ValMinMax("Solar Array -Y Deployable -", String.format(MILLI_AMPS_FORMAT, realTimeFC2.getSolarArrayCurrent4MinusString()), N_A, N_A));
-		epsValues.add(new ValMinMax("Solar Array X Fixed +", String.format(MILLI_AMPS_FORMAT, realTimeFC2.getSolarArrayCurrent5PlusString()), N_A, N_A));
-		epsValues.add(new ValMinMax("Solar Array X Fixed -", String.format(MILLI_AMPS_FORMAT, realTimeFC2.getSolarArrayCurrent5MinusString()), N_A, N_A));
-		epsValues.add(new ValMinMax("Solar Array +X Deployable +", String.format(MILLI_AMPS_FORMAT, realTimeFC2.getSolarArrayCurrent6PlusString()), N_A, N_A));
-		epsValues.add(new ValMinMax("Solar Array +X Deployable -", String.format(MILLI_AMPS_FORMAT, realTimeFC2.getSolarArrayCurrent6MinusString()), N_A, N_A));
+		String minValue = "";
+        String maxValue = "";
+        
+        minValue = generateSolarArrayCurrentString(minmaxValues.get(0).getMinimum(), 0);
+        maxValue = generateSolarArrayCurrentString(minmaxValues.get(0).getMaximum(), 0);
+		epsValues.add(new ValMinMax("Solar Array +Y Deployable +", String.format(MILLI_AMPS_FORMAT, realTimeFC2.getSolarArrayCurrent1PlusString()), maxValue, minValue));
+
+        minValue = generateSolarArrayCurrentString(minmaxValues.get(1).getMinimum(), 0);
+        maxValue = generateSolarArrayCurrentString(minmaxValues.get(1).getMaximum(), 0);
+		epsValues.add(new ValMinMax("Solar Array +Y Deployable -", String.format(MILLI_AMPS_FORMAT, realTimeFC2.getSolarArrayCurrent1MinusString()), maxValue, minValue));
+        
+		minValue = generateSolarArrayCurrentString(minmaxValues.get(2).getMinimum(), 0);
+        maxValue = generateSolarArrayCurrentString(minmaxValues.get(2).getMaximum(), 0);
+		epsValues.add(new ValMinMax("Solar Array Y Fixed +", String.format(MILLI_AMPS_FORMAT, realTimeFC2.getSolarArrayCurrent2PlusString()), maxValue, minValue));
+
+        minValue = generateSolarArrayCurrentString(minmaxValues.get(3).getMinimum(), 0);
+        maxValue = generateSolarArrayCurrentString(minmaxValues.get(3).getMaximum(), 0);
+		epsValues.add(new ValMinMax("Solar Array Y Fixed -", String.format(MILLI_AMPS_FORMAT, realTimeFC2.getSolarArrayCurrent2MinusString()), maxValue, minValue));
 		
-		epsValues.add(new ValMinMax("Battery 0 Current", String.format(MILLI_AMPS_FORMAT, realTimeFC2.getBattery0CurrentString()), N_A, N_A));
-		epsValues.add(new ValMinMax("Battery 0 Voltage", String.format(VOLTS_FORMAT, realTimeFC2.getBattery0VoltsString()), N_A, N_A));
-		epsValues.add(new ValMinMax("Battery 0 Temperature", String.format(TEMPERATURE_FORMAT, realTimeFC2.getBattery0TemperatureString()), N_A, N_A));
-		epsValues.add(new ValMinMax("Battery 1 Current", String.format(MILLI_AMPS_FORMAT, realTimeFC2.getBattery1CurrentString()), N_A, N_A));
-		epsValues.add(new ValMinMax("Battery 1 Voltage", String.format(VOLTS_FORMAT, realTimeFC2.getBattery1VoltsString()), N_A, N_A));
-		epsValues.add(new ValMinMax("Battery 1 Temperature", String.format(TEMPERATURE_FORMAT, realTimeFC2.getBattery1TemperatureString()), N_A, N_A));
-		epsValues.add(new ValMinMax("Battery 2 Current", String.format(MILLI_AMPS_FORMAT, realTimeFC2.getBattery2CurrentString()), N_A, N_A));
-		epsValues.add(new ValMinMax("Battery 2 Voltage", String.format(VOLTS_FORMAT, realTimeFC2.getBattery2VoltsString()), N_A, N_A));
-		epsValues.add(new ValMinMax("Battery 2 Temperature", String.format(TEMPERATURE_FORMAT, realTimeFC2.getBattery2TemperatureString()), N_A, N_A));
+		minValue = generateSolarArrayCurrentString(minmaxValues.get(4).getMinimum(), 0);
+        maxValue = generateSolarArrayCurrentString(minmaxValues.get(4).getMaximum(), 0);
+        epsValues.add(new ValMinMax("Solar Array Z Fixed +", String.format(MILLI_AMPS_FORMAT, realTimeFC2.getSolarArrayCurrent3PlusString()), maxValue, minValue));
+		
+        minValue = generateSolarArrayCurrentString(minmaxValues.get(5).getMinimum(), 0);
+        maxValue = generateSolarArrayCurrentString(minmaxValues.get(5).getMaximum(), 0);
+        epsValues.add(new ValMinMax("Solar Array Z Fixed -", String.format(MILLI_AMPS_FORMAT, realTimeFC2.getSolarArrayCurrent3MinusString()), maxValue, minValue));
+		
+        minValue = generateSolarArrayCurrentString(minmaxValues.get(6).getMinimum(), 0);
+        maxValue = generateSolarArrayCurrentString(minmaxValues.get(6).getMaximum(), 0);
+        epsValues.add(new ValMinMax("Solar Array -Y Deployable +", String.format(MILLI_AMPS_FORMAT, realTimeFC2.getSolarArrayCurrent4PlusString()), maxValue, minValue));
+		
+        minValue = generateSolarArrayCurrentString(minmaxValues.get(7).getMinimum(), 0);
+        maxValue = generateSolarArrayCurrentString(minmaxValues.get(7).getMaximum(), 0);
+        epsValues.add(new ValMinMax("Solar Array -Y Deployable -", String.format(MILLI_AMPS_FORMAT, realTimeFC2.getSolarArrayCurrent4MinusString()), maxValue, minValue));
+		
+        minValue = generateSolarArrayCurrentString(minmaxValues.get(8).getMinimum(), 0);
+        maxValue = generateSolarArrayCurrentString(minmaxValues.get(8).getMaximum(), 0);
+        epsValues.add(new ValMinMax("Solar Array X Fixed +", String.format(MILLI_AMPS_FORMAT, realTimeFC2.getSolarArrayCurrent5PlusString()), maxValue, minValue));
+		
+        minValue = generateSolarArrayCurrentString(minmaxValues.get(9).getMinimum(), 0);
+        maxValue = generateSolarArrayCurrentString(minmaxValues.get(9).getMaximum(), 0);
+        epsValues.add(new ValMinMax("Solar Array X Fixed -", String.format(MILLI_AMPS_FORMAT, realTimeFC2.getSolarArrayCurrent5MinusString()), maxValue, minValue));
+		
+        minValue = generateSolarArrayCurrentString(minmaxValues.get(10).getMinimum(), 0);
+        maxValue = generateSolarArrayCurrentString(minmaxValues.get(10).getMaximum(), 0);
+        epsValues.add(new ValMinMax("Solar Array +X Deployable +", String.format(MILLI_AMPS_FORMAT, realTimeFC2.getSolarArrayCurrent6PlusString()), maxValue, minValue));
+		
+        minValue = generateSolarArrayCurrentString(minmaxValues.get(11).getMinimum(), 0);
+        maxValue = generateSolarArrayCurrentString(minmaxValues.get(11).getMaximum(), 0);
+        epsValues.add(new ValMinMax("Solar Array +X Deployable -", String.format(MILLI_AMPS_FORMAT, realTimeFC2.getSolarArrayCurrent6MinusString()), maxValue, minValue));
+		
+        // NOTE SKIP 13 Solar Panel Temp
+        
+        minValue = generateBatteryCurrentString(minmaxValues.get(13).getMinimum());
+        maxValue = generateBatteryCurrentString(minmaxValues.get(13).getMaximum());
+		epsValues.add(new ValMinMax("Battery 0 Current", String.format(MILLI_AMPS_FORMAT, realTimeFC2.getBattery0CurrentString()), maxValue, minValue));
+		
+		minValue = generateVoltageString(minmaxValues.get(14).getMinimum());
+		maxValue = generateVoltageString(minmaxValues.get(14).getMaximum());
+		epsValues.add(new ValMinMax("Battery 0 Voltage", String.format(VOLTS_FORMAT, realTimeFC2.getBattery0VoltsString()), maxValue, minValue));
+		
+		minValue = generateBatteryTemperatureString(minmaxValues.get(15).getMinimum());
+		maxValue = generateBatteryTemperatureString(minmaxValues.get(15).getMaximum());
+        epsValues.add(new ValMinMax("Battery 0 Temperature", String.format(TEMPERATURE_FORMAT, realTimeFC2.getBattery0TemperatureString()), maxValue, minValue));
+        
+        minValue = generateBatteryCurrentString(minmaxValues.get(16).getMinimum());
+        maxValue = generateBatteryCurrentString(minmaxValues.get(16).getMaximum());
+        epsValues.add(new ValMinMax("Battery 1 Current", String.format(MILLI_AMPS_FORMAT, realTimeFC2.getBattery1CurrentString()), maxValue, minValue));
+        
+        minValue = generateVoltageString(minmaxValues.get(17).getMinimum());
+        maxValue = generateVoltageString(minmaxValues.get(17).getMaximum());
+        epsValues.add(new ValMinMax("Battery 1 Voltage", String.format(VOLTS_FORMAT, realTimeFC2.getBattery1VoltsString()), maxValue, minValue));
+		
+		minValue = generateBatteryTemperatureString(minmaxValues.get(18).getMinimum());
+		maxValue = generateBatteryTemperatureString(minmaxValues.get(18).getMaximum());
+        epsValues.add(new ValMinMax("Battery 1 Temperature", String.format(TEMPERATURE_FORMAT, realTimeFC2.getBattery1TemperatureString()), maxValue, minValue));
+		
+		minValue = generateBatteryCurrentString(minmaxValues.get(19).getMinimum());
+        maxValue = generateBatteryCurrentString(minmaxValues.get(19).getMaximum());
+        epsValues.add(new ValMinMax("Battery 2 Current", String.format(MILLI_AMPS_FORMAT, realTimeFC2.getBattery2CurrentString()), maxValue, minValue));
+        
+        minValue = generateVoltageString(minmaxValues.get(20).getMinimum());
+        maxValue = generateVoltageString(minmaxValues.get(20).getMaximum());
+        epsValues.add(new ValMinMax("Battery 2 Voltage", String.format(VOLTS_FORMAT, realTimeFC2.getBattery2VoltsString()), maxValue, minValue));
+		
+		minValue = generateBatteryTemperatureString(minmaxValues.get(21).getMinimum());
+		maxValue = generateBatteryTemperatureString(minmaxValues.get(21).getMaximum());
+        epsValues.add(new ValMinMax("Battery 2 Temperature", String.format(TEMPERATURE_FORMAT, realTimeFC2.getBattery2TemperatureString()), maxValue, minValue));
+        
 		epsValues.add(new ValMinMax("Auto Thermal Control", realTimeFC2.getBatteryHeaterForcedOnString(), N_A, N_A));
 		
 		amacValues.add(new ValMinMax("Controller Mode", realTimeFC2.getAmacModeString(), N_A, N_A));
@@ -171,15 +241,33 @@ public class RealTimeFC2ServiceRestImpl extends AbstractService {
 		
 		rfValues.add(new ValMinMax("Receiver Doppler", realTimeFC2.getReceiverDopplerString(), N_A, N_A));
 		rfValues.add(new ValMinMax("Receiver RSSI", realTimeFC2.getReceiverRSSIString(), N_A, N_A));
-		rfValues.add(new ValMinMax("Temperature", String.format(TEMPERATURE_FORMAT, realTimeFC2.getReceiverTemperatureString()), N_A, N_A));
-		rfValues.add(new ValMinMax("Receive Current", String.format(MILLI_AMPS_FORMAT, realTimeFC2.getReceiverCurrentString()), N_A, N_A));
-		rfValues.add(new ValMinMax("Transmit Current 3.3V bus", String.format(MILLI_AMPS_FORMAT, realTimeFC2.getTransmitCurrent3v3String()), N_A, N_A));
-		rfValues.add(new ValMinMax("Transmit Current 5.0V bus", String.format(MILLI_AMPS_FORMAT, realTimeFC2.getTransmitCurrent5v0String()), N_A, N_A));
+        
+        minValue = generateReceiverTemperatureString(minmaxValues.get(30).getMinimum());
+        maxValue = generateReceiverTemperatureString(minmaxValues.get(30).getMaximum());
+        rfValues.add(new ValMinMax("Temperature", String.format(TEMPERATURE_FORMAT, realTimeFC2.getReceiverTemperatureString()), maxValue, minValue));
+		
+		minValue = generateReceiverCurrentString(minmaxValues.get(31).getMinimum());
+        maxValue = generateReceiverCurrentString(minmaxValues.get(31).getMaximum());
+		rfValues.add(new ValMinMax("Receive Current", String.format(MILLI_AMPS_FORMAT, realTimeFC2.getReceiverCurrentString()), minValue, maxValue));
+		
+		minValue = generateTransmitCurrent3v3String(minmaxValues.get(32).getMinimum());
+        maxValue = generateTransmitCurrent3v3String(minmaxValues.get(32).getMaximum());
+        rfValues.add(new ValMinMax("Transmit Current 3.3V bus", String.format(MILLI_AMPS_FORMAT, realTimeFC2.getTransmitCurrent3v3String()), minValue, maxValue));
+        
+        minValue = generateTransmitCurrent5v0String(minmaxValues.get(33).getMinimum());
+        maxValue = generateTransmitCurrent5v0String(minmaxValues.get(33).getMaximum());
+        rfValues.add(new ValMinMax("Transmit Current 5.0V bus", String.format(MILLI_AMPS_FORMAT, realTimeFC2.getTransmitCurrent5v0String()), minValue, maxValue));
 		
 		paValues.add(new ValMinMax("Forward Power", String.format(PA_MILLI_WATT_FORMAT, realTimeFC2.getForwardPowerString()), N_A, N_A));
 		paValues.add(new ValMinMax("Reverse Power", String.format(PA_MILLI_WATT_FORMAT, realTimeFC2.getReversePowerString()), N_A, N_A));
-		paValues.add(new ValMinMax("Device Temperature", String.format(PA_TEMPERATURE_FORMAT, realTimeFC2.getPaDeviceTemperatureString()), N_A, N_A));
-		paValues.add(new ValMinMax("Bus Current", String.format(PA_MILLI_AMPS_FORMAT, realTimeFC2.getPaBusCurrentString()), N_A, N_A));
+        
+        minValue = generatePaDeviceTemperatureString(minmaxValues.get(36).getMinimum());
+        maxValue = generatePaDeviceTemperatureString(minmaxValues.get(36).getMaximum());
+        paValues.add(new ValMinMax("Device Temperature", String.format(PA_TEMPERATURE_FORMAT, realTimeFC2.getPaDeviceTemperatureString()), maxValue, minValue));
+        
+        minValue = generatePaBusCurrentString(minmaxValues.get(37).getMinimum());
+        maxValue = generatePaBusCurrentString(minmaxValues.get(37).getMaximum());
+        paValues.add(new ValMinMax("Bus Current", String.format(PA_MILLI_AMPS_FORMAT, realTimeFC2.getPaBusCurrentString()), minValue, maxValue));
 		
 		swValues.add(new StringPair("In Eclipse", (realTimeFC2.isEclipsed()) ? "No" : "Yes"));
 		swValues.add(new StringPair("Mode Manager Mode", realTimeFC2.getModeManagerModeString()));
@@ -196,15 +284,91 @@ public class RealTimeFC2ServiceRestImpl extends AbstractService {
 		swValues.add(new StringPair("Last Command", realTimeFC2.getDtmfLastCommandString()));
 		swValues.add(new StringPair("Command Success", (realTimeFC2.getDtmfCommandSuccess()) ? "Yes" : "No"));
 		
-		SharedInfo realtimeInfo 
+		final int valid = realTimeEntity.isValid() ? 1 : 0;
+		
+        SharedInfo realtimeInfo 
 			= new RealTimeFC2Info(realTimeEntity.getSequenceNumber(), 
 					SDTF.format(createdDate),
 					epsValues, amacValues, rfValues, paValues, swValues,
 					siteList, SDTF.format(minmaxResetDate),
 					latitude, longitude, packetCount, satelliteMode, transponderState,
-					realTimeEntity.isValid() ? 1 : 0);
+					valid);
 		
 		return realtimeInfo;
 		
 	}
+    
+    private String generatePaBusCurrentString(Long paBoardCurrent) {
+        return String.format("%5.1f", (paBoardCurrent * 5.496E-01) + 2.544E+00);
+    }
+
+    private String generatePaDeviceTemperatureString(Long paBoardTemperature) {
+        double value = 6.29764694373E+001 +
+                (3.80132039683E-001 * paBoardTemperature) +
+                (-9.59767044686E-003 * paBoardTemperature * paBoardTemperature) +
+                (5.15917746352E-005 * paBoardTemperature * paBoardTemperature * paBoardTemperature) +
+                (-9.77975220499E-008 * paBoardTemperature * paBoardTemperature * paBoardTemperature * paBoardTemperature);
+        
+        return String.format("%5.1f", value);
+    }
+
+    private String generateTransmitCurrent5v0String(Long transmitCurrent5v0) {
+        return String.format("%5.1f", transmitCurrent5v0 * 0.636);
+    }
+
+    private String generateTransmitCurrent3v3String(Long transmitCurrent3v3) {
+        return String.format("%5.1f", transmitCurrent3v3 * 0.636);
+    }
+
+    private String generateReceiverCurrentString(Long receiverCurrent) {
+        return String.format("%5.1f", receiverCurrent * 0.636);
+    }
+
+    private String generateReceiverTemperatureString(Long receiverTemperature) {
+        return String.format("%5.1f", (receiverTemperature * -0.857) + 193.672);
+    }
+
+    private String generateSolarArrayCurrentString(Long current, int type) {
+        if (current == 99999 || current == -99999) {
+            return "N/A";
+        }
+        if (current == 1023) {
+            return "0";
+        } else {
+            if (type == 0) {
+                return String.format("%3.1f", (current * -0.54312) +  + 555.61);
+            } else {
+                return String.format("%3.1f", (current * -0.575) +  + 588.22);
+            }
+        }
+    }
+
+    private String generateBatteryCurrentString(Long current) {
+        if (current == 99999 || current == -99999) {
+            return "N/A";
+        }
+        if (current == 0) {
+            return "0";
+        } else {
+            return String.format("%4.0f", ((current * 4 * -3.200E+00) + 2.926E+03));
+        }
+    }
+
+    private String generateVoltageString(Long volts) {
+        if (volts == 99999 || volts == -99999) {
+            return "N/A";
+        }
+        if (volts == 0) {
+            return "0";
+        } else {
+            return String.format("%4.1f", (volts * 4 * -9.470E-03) + 9.798896E+00);
+        }
+    }
+    
+    private String generateBatteryTemperatureString(Long temperature) {
+        if (temperature == 99999 || temperature == -99999) {
+            return "N/A";
+        }
+        return String.format("%3.1f", (temperature * -0.163 * 4.75) + 75);
+    }
 }
